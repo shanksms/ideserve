a040efa07f5e1df2bfb019d64eece11ecb9eb46b
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2017-10-02 02:36:44 +0530

    corrected code of inorder and postorder traversal

--- binary_tree/binary_tree_traversal.py
+++ binary_tree/binary_tree_traversal.py
@@ -10,31 +10,30 @@ def pre_order_traversal(root):
 def in_order_traversal(root):
 
     if root.left != None:
-         pre_order_traversal(root.left)
+         in_order_traversal(root.left)
     print(root.data, end=' ')
 
     if root.right != None:
-         pre_order_traversal(root.right)
+         in_order_traversal(root.right)
 def post_order_traversal(root):
     if root.left != None:
-         pre_order_traversal(root.left)
+         post_order_traversal(root.left)
 
     if root.right != None:
-         pre_order_traversal(root.right)
+         post_order_traversal(root.right)
     print(root.data, end=' ')
 
 
 if __name__ == '__main__':
-
-
-    node1 = BinaryTree(None, None, 1)
-    node2 = BinaryTree(None, None, 2)
-    root = BinaryTree(node1, node2, 3)
+    root = BinaryTree(
+    BinaryTree(BinaryTree(None, None, 4), BinaryTree(None, None, 5), 2),
+    BinaryTree(None, None, 3), 1)
     #if node1.left == None:
     #    print('Is none')
     '''
-        3
-     1     2
+            1
+      2              3
+ 4        5
     '''
     pre_order_traversal(root)
     # 3 1 2
