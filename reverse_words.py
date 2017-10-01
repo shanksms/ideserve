e4e5705abb5b04eada825803171f05b8acd791cb
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2017-10-01 19:01:55 +0530

    reverse words code

--- reverse_words.py
+++ reverse_words.py
@@ -1,1 +1,10 @@
 #write code
+
+
+def reverse_words(input_words):
+    word_list = input_words.split(' ')
+    word_list.reverse()
+    print(" ".join(word_list))
+
+if __name__ == '__main__':
+    reverse_words(input())
