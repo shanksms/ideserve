1c740ae1b7450f294f3068a2f9dc8614eff0cfed
sriv.srivastava@gmail.com <sriv.srivastava@gmail.com>
2017-10-02 13:14:29 +0530

    implemented level order traversal

--- binary_tree/binary_tree_traversal.py
+++ binary_tree/binary_tree_traversal.py
@@ -22,6 +22,26 @@ def post_order_traversal(root):
     if root.right != None:
          post_order_traversal(root.right)
     print(root.data, end=' ')
+def level_order_traversal(root):
+    level_elements_dict = {}
+    level_order_traversal_helper(root, 0, level_elements_dict)
+    for key, value in level_elements_dict.items():
+        ls_of_str = [str(i) for i in value]
+        print(" ".join(ls_of_str), end=' ')
+def level_order_traversal_helper(root, level, level_elements_dict):
+    if level in level_elements_dict:
+        level_elements_dict[level].append(root.data)
+    else:
+        element_list = []
+        element_list.append(root.data)
+        level_elements_dict[level] = element_list
+    if root.left != None:
+        level_order_traversal_helper(root.left, level + 1, level_elements_dict)
+    if root.right != None:
+        level_order_traversal_helper(root.right, level + 1, level_elements_dict)
+
+
+
 
 
 if __name__ == '__main__':
@@ -35,11 +55,11 @@ if __name__ == '__main__':
       2              3
  4        5
     '''
-    pre_order_traversal(root)
-    # 3 1 2
-    print()
-    # 1 3 2
-    in_order_traversal(root)
-    print()
-    post_order_traversal(root)
+    #pre_order_traversal(root)
+    #print()
+    #in_order_traversal(root)
+    #print()
+    #post_order_traversal(root)
+    #print()
+    level_order_traversal(root)
     print()
